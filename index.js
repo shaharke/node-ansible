0592acbf16c8c44b523e2cc548901054bdfb88f4
Shahar Kedar <shahar@bigpanda.io>
2014-01-18 11:43:53 +0200

    Minimal version of execution of AdHoc commands

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.Ansible = {
+  AdHoc: require('./lib/ansible').AdHoc,
+  Playbook: require('./lib/ansible').Playbook
+};
---
+++ test/adhoc.spec.js
@@ -0,0 +1,55 @@
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var execSpy = sinon.spy(require('shelljs'), 'exec');
+
+var sinonChai = require("sinon-chai");
+var chaiAsPromised = require('chai-as-promised');
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('AdHoc command', function() {
+
+  var AdHoc = require("../index").Ansible.AdHoc;
+
+  describe('with no structured args and freeform arg', function() {
+
+    it('should be translated successfully to ansible command', function(done) {
+      var command = new AdHoc().module('shell').hosts('local').args(null, "echo 'hello'");
+      expect(command.exec()).to.be.fulfilled.then(function() {
+        expect(execSpy).to.be.calledWith('ansible local -m shell -a "echo \'hello\'"');
+        done();
+      }).done();
+    })
+
+  })
+
+  describe('with no hosts', function() {
+
+    it('should be rejected', function() {
+      var command = new AdHoc().module('shell').args(null, "echo 'hello'");
+      expect(command.exec()).to.be.rejected;
+    })
+
+    it.only('should include reason in rejection', function(done) {
+      var command = new AdHoc().module('shell').args(null, "echo 'hello'");
+      expect(command.exec()).to.be.rejected.then(function(error) {
+        expect(error).to.have.property('reason');
+        expect(error.reason).to.be.array;
+        expect(error.reason).to.have.length(1);
+        done();
+      })
+    })
+
+  })
+
+  describe('with no module', function() {
+
+    it('should be rejected', function() {
+      var command = new AdHoc().hosts('local').args(null, "echo 'hello'");
+      expect(command.exec()).to.be.rejected;
+    })
+
+  })
+
+})
